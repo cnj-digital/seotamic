ec8987656f9f8e19381d902bf6769bad2143c444
Martin <martin.koterle@gmail.com>
2023-05-02 17:39:47 +0200

    wip

--- src/routes/cp.php
+++ src/routes/cp.php
@@ -1,6 +1,8 @@
 <?php
 
+use Cnj\Seotamic\Http\Controllers\SettingsController;
+
 Route::middleware('statamic.cp.authenticated')->group(function () {
-    Route::get('/cnj/seotamic/', 'SettingsController@index')->name('cnj.seotamic.index');
-    Route::post('/cnj/seotamic/', 'SettingsController@update')->name('cnj.seotamic.update');
+    Route::get('/cnj/seotamic/', [SettingsController::class, 'index'])->name('cnj.seotamic.index');
+    Route::post('/cnj/seotamic/', [SettingsController::class, 'update'])->name('cnj.seotamic.update');
 });
