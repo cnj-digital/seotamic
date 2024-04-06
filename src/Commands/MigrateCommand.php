2a35a18fc0d90c29b74edd1351acb2e52e37a11d
Martin <martin.koterle@gmail.com>
2024-04-06 08:19:19 +0200

    Fix

--- src/Commands/MigrateCommand.php
+++ src/Commands/MigrateCommand.php
@@ -54,7 +54,13 @@ class MigrateCommand extends Command
         });
 
         $blueprintsWithSeotamicFields = $blueprints->filter(function ($blueprint) {
-            return $blueprint->hasSection('SEO') || $blueprint->hasSection('Social');
+            if (method_exists($blueprint, 'hasSection')) {
+                return $blueprint->hasSection('SEO') || $blueprint->hasSection('Social');
+            }
+
+            if (method_exists($blueprint, 'hasTab')) {
+                return $blueprint->hasTab('SEO') || $blueprint->hasTab('Social');
+            }
         });
 
         $skippedBlueprints = [];
