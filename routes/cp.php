8f8d8d21c442ab6c473efc76b8034e3653004ad7
Martin <martin.koterle@gmail.com>
2026-03-26 11:27:16 +0100

    fix: add permission middleware to settings routes (#110)

--- routes/cp.php
+++ routes/cp.php
@@ -2,7 +2,7 @@
 
 use Cnj\Seotamic\Http\Controllers\SettingsController;
 
-Route::middleware('statamic.cp.authenticated')->group(function () {
+Route::middleware(['statamic.cp.authenticated', 'can:view seotamic tool'])->group(function () {
     Route::get('/cnj/seotamic', [SettingsController::class, 'index'])->name('cnj.seotamic.index');
     Route::post('/cnj/seotamic', [SettingsController::class, 'update'])->name('cnj.seotamic.update');
 });
